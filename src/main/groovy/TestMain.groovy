1080567f9554fb9a1d76a79d68043eaea2f6b883
muumin <muu0607@gmail.com>
2013-02-12 12:29:23 +0900

    gradle sample code.

---
+++ src/main/groovy/TestMain.groovy
@@ -0,0 +1,11 @@
+/**
+ * ClassComment
+ * User: muu
+ * Date: 13/02/12
+ * Time: 11:50
+ */
+class TestMain {
+    public static void main(String[] args) {
+        println('Hello World!!')
+    }
+}
